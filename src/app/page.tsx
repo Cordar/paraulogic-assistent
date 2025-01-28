6c43fe8b1a34c542751f805ffda7937dfc837736
Cordar <enricmir1993@gmail.com>
2025-01-29 00:15:15 +0100

    fixed eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 
 
 
@@ -10,10 +10,10 @@ export default function Home() {
   const [subconjunts, setSubconjunts] = useState<{ subconjunt: string, repeticions: number }[]>([]);
   const [prefixos, setPrefixos] = useState<{ prefix: string, repeticions: number }[]>([]);
 
-  function handleKeyPress(e: any) {
+  function handleKeyPress(e: { key: string; }) {
     if (e.key == "Enter") {
       const paraulesIntroduides = paraula.split(",");
-      let paraulesPerAfegir: string[] = [];
+      const paraulesPerAfegir: string[] = [];
       for (let index = 0; index < paraulesIntroduides.length; index++) {
         let paraulaNeta = paraulesIntroduides[index];
         paraulaNeta = paraulaNeta.replace("-", "").replace(".", "")
@@ -31,21 +31,21 @@ export default function Home() {
     }
   }
 
-  function handleInputChange(e: any) {
+  function handleInputChange(e: { target: { value: SetStateAction<string>; }; }) {
     setParaula(e?.target?.value);
   }
 
   function updateSubConjunts() {
-    let localSubconjunts: { subconjunt: string, repeticions: number }[] = [];
+    const localSubconjunts: { subconjunt: string, repeticions: number }[] = [];
     for (let index = 0; index < paraules.length; index++) {
       const paraula = paraules[index];
 
-      let localSubconjuntSet = new Set<string>();
+      const localSubconjuntSet = new Set<string>();
       for (let index = 0; index < paraula.length; index++) {
         localSubconjuntSet.add(paraula[index]);
       }
-      let localSubconjuntArray = Array.from(localSubconjuntSet).sort();
-      let localSubconjunt = localSubconjuntArray.join("");
+      const localSubconjuntArray = Array.from(localSubconjuntSet).sort();
+      const localSubconjunt = localSubconjuntArray.join("");
       const repIndex = localSubconjunts.findIndex(sc => sc.subconjunt == localSubconjunt);
       if (repIndex == -1) {
         localSubconjunts.push({ subconjunt: localSubconjunt, repeticions: 1 })
@@ -58,7 +58,7 @@ export default function Home() {
   }
 
   function updatePrefixes() {
-    let localPrefixos: { prefix: string, repeticions: number }[] = [];
+    const localPrefixos: { prefix: string, repeticions: number }[] = [];
     for (let index = 0; index < paraules.length; index++) {
       const paraula = paraules[index];
 
@@ -81,10 +81,10 @@ export default function Home() {
 
   return (
     <div className="w-1/2 m-auto p-16 text-center flex flex-col">
-      <h1 className="mt-2 text-2xl">Benvingut a l'assistent del paraulògic</h1>
+      <h1 className="mt-2 text-2xl">Benvingut a l`&apos;`assistent del paraulògic</h1>
       <h2 className="mt-16 mb-2 text-lg">Introdueix una paraula</h2>
       <div className="flex">
-        <button onClick={fesInforme} className="w-1/4">Fes l'informe</button>
+        <button onClick={fesInforme} className="w-1/4">Fes l`&apos;`informe</button>
         <input type="text" onKeyUp={handleKeyPress} value={paraula} onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="t'estimo"></input>
       </div>
       <div className="mt-2 flex flex-wrap w-3/4 place-self-end">
